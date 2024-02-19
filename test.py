69a1263f12b2cf04e7119d2eb7ea071b8c5cb24b
Durga Kalyan Ganjapu <dkalze@gmail.com>
2024-02-19 15:49:35 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import os, sys
+import logging
+
+logging.info(f"Started")
+x = pd.DataFrame({'a':[1,2,3], 'b':['x','y','z']})
+logging.info(x)
+logging.info("Done!")
