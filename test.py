24b4a1500ce1203127f025e0348251cfc6c9edea
Durga Kalyan Ganjapu <dkalze@gmail.com>
2024-02-19 21:01:26 +0000

    test python file

--- test.py
+++ test.py
@@ -3,7 +3,10 @@ import numpy as np
 import os, sys
 import logging
 
-logging.info(f"Started")
+logging.basicConfig(level=logging.DEBUG)
+logging.info(f"Started, python: {sys.version_info}")
+logging.info(f"pd version: {pd.__version__}")
+logging.info(f"np version: {np.__version__}")
 x = pd.DataFrame({'a':[1,2,3], 'b':['x','y','z']})
 logging.info(x)
-logging.info("Done!")
+logging.warning("Done!")
